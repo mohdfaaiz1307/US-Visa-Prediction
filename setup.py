df048c8f90a545e600a0f0f6be2dc0302ceda64b
mohdfaaiz1307 <mohdfaaiz1307@gmail.com>
2024-09-21 11:30:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US-Visa",
+    version="0.0.0",
+    author="mohdfaaiz1307",
+    author_email="mohdfaaiz1307@gmail.com",
+    packages=find_packages()
+)
